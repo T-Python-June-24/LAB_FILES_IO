825fe3c3678bcb6c31e254ac4b156f0ae45f1701
amira alrshidi <ameerah.alrashidi0111@gmail.com>
2024-06-24 14:09:42 +0300

    edit solution of to-do list lab

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,4 +1,4 @@
-import os 
+import os   
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -10,35 +10,53 @@ def main():
     ---------------------------------------------    
         Welcome in To-Do List app 🗓️ ✅                   
     ---------------------------------------------    
-        \033[34m1- View To-do list 🗓️
+        \033[34m1- Add new To-Do item 📥
               
-        2- Add new To-Do item 📥
+        2- View To-do list 🗓️
               
         3- exit 👋
               ''')
-        
+
         userInput = input("Enter a number from the above menu: ")
+
+        if userInput == "1" :
+                while True:
+
+                    newItem = input("\nPlease enter the new item to store in your To-Do List enter 3 to exit : ")
+                    if newItem == "3":
+                        break
+                    else:
+                    # open a to-do list file and write in it newitme then save it 
+                        with open('To_Do_List.txt','a') as file :
+                            file.write(newItem+"\n")
+                        print(f'''\033[32m 
+                            
+                            new item '{newItem}' added sccessfully to your To-Do List 
+
+                            \033[34m''')
         
-        if userInput == "1":
-                # read a to-do list file and preant item line by line
-            with open('To_Do_List.txt','r',encoding="utf-8") as file :
+        elif userInput == "2":
 
-                print(f'''\033[32m
-{file.read()}\033[34m''')
-
-            input("Press enter to continue..")
-
-        elif userInput == "2" :
-            newItem = input("\nPlease enter the new item to store in your To-Do List: ")
-            # open a to-do list file and write in it newitme then save it 
-            with open('To_Do_List.txt','a') as file :
-                file.write(newItem+"\n")
-            print(f'''\033[32m 
+            if 'To_Do_List.txt' not in os.listdir():
+                    
+                print(f'''\033[31m 
+        Error: You do not have a To-Do list 
+        \033[34m''')
                 
-                new item '{newItem}' added sccessfully to your To-Do List 
+                input("Press enter to continue..")
+                
+            else:
+                    # read a to-do list file and preant item line by line
+                with open('To_Do_List.txt','r',encoding="utf-8") as file :
+                    file.seek(0)
+                    
+                    toDoList = list(file.readlines())
+                    for i in toDoList:
+                        file.seek(0)
+                        print(f'''\033[32m
+        {i}\033[34m''')
 
-                \033[34m''')
-            input("Press enter to continue..")
+                input("Press enter to continue..")
 
         elif userInput == "3": 
             print(f'''\033[33m 
