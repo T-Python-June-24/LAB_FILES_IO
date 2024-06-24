b70fa1042b09d1f1efab99ad5fd0ff3bd4fcc9a5
D. F <ifonei@hotmail.com>
2024-06-24 13:25:41 +0300

    Lab Filed_io-- to do list finished

--- toDoList.py
+++ toDoList.py
@@ -2,15 +2,12 @@ def readFile():
     file = open("to_do.txt", "r", encoding = "UTF-8")
     file.seek(0)
     content = file.read()
-    items = content.split(" ")
-    print("Your items in To Do List are:")
-    for item in items:
-        print(item)
+    print(content)
     file.close()
 
 
 def writeToFile(content):
     file = open("to_do.txt", "a+", encoding = "UTF-8")
-    file.write(content)
+    file.write(content + "\n")
     file.close()
 
